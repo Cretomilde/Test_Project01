333e2a007ff540e9943dfd44b4b0b08bf3f86e43
Paulo Cruz <paulo.cruz@tecnico.ulisboa.pt>
2021-03-03 21:34:15 +0000

    ssss

--- projects/test_project01/teste.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-    printf("\n\nPUTA QUE PARIU ESTA MERDA FODA-SE\n\n");
-    printf("\n\nFILHAS DAS PUTAS ACHO QUE CONSEGUI\n\n");
-    //YEEEEEEEEEEEEEEESSSSSSSSSSSSSSSSSSSSS
-    return 0;
-}
