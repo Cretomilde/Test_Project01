e753890dfcd63f2dd013b06df5c982c0a7746d1a
Paulo Cruz <paulo.cruz@tecnico.ulisboa.pt>
2021-03-03 21:32:06 +0000

    nocnoc

--- projects/test_project01/teste.c
+++ projects/test_project01/teste.c
@@ -4,5 +4,6 @@
 int main(){
     printf("\n\nPUTA QUE PARIU ESTA MERDA FODA-SE\n\n");
     printf("\n\nFILHAS DAS PUTAS ACHO QUE CONSEGUI\n\n");
+    //YEEEEEEEEEEEEEEESSSSSSSSSSSSSSSSSSSSS
     return 0;
 }
