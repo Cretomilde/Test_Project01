99841ca981f9e92a799947e51f3509f92747e942
Paulo Cruz <paulo.cruz@tecnico.ulisboa.pt>
2021-03-04 23:47:12 +0000

    nice

--- teste2.c
+++ teste2.c
@@ -6,3 +6,6 @@
 //sem ter de clonar. afinal podes editar mas depois tens de abrir para submeter(commit e push)
 // comentario 
 //ja deu
+/*descobri outra cena. se fizeres "code ." dentro da pasta projects, ao abrires repositório nao aparecem aquelas changes todas, mas so 
+as que estão dentro de project. é  muito bom. basicamente, no terminal, tens de fazer primeiro "cd projects" e so depois "code ."*/
+//coiso
