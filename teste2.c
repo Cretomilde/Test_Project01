e36eb103548469915a686fa59c685b32687868c7
Paulo Cruz <paulo.cruz@tecnico.ulisboa.pt>
2021-03-04 08:59:25 +0000

    good shit

--- teste2.c
+++ teste2.c
@@ -2,3 +2,5 @@
 //Git: Close Repository
 //Git: Open Repository
 //Editei isto, e só depois da edição é que abri o repositório
+//Editei isto outra vez sem abrir repositório e
+//sem ter de clonar. afinal podes editar mas depois tens de abrir para submeter(commit e push)
