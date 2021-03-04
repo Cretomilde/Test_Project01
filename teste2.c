6a7aa9264de3f612b11afbabad764cda173dce91
rui_ferreira <rui.silva.ferreira@tecnico.ulisboa.pt>
2021-03-04 11:19:10 +0000

    v8

--- teste2.c
+++ teste2.c
@@ -4,4 +4,4 @@
 //Editei isto, e só depois da edição é que abri o repositório
 //Editei isto outra vez sem abrir repositório e
 //sem ter de clonar. afinal podes editar mas depois tens de abrir para submeter(commit e push)
-// comentario
+// comentario 
